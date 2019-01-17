14f40c001c09f769d7d0036992c2b364cdbcff9c
sofiabsilva <sofi.bsilva@gmail.com>
2019-01-17 16:21:52 +0000

    add player and current song info script, adjust styling

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+const player = document.querySelector('.player')
+const currentSection = document.querySelector('.currentSection')
+player.addEventListener('click', toggleAudio)
+
+window.onload = currentSongs;
+
+let audio = new Audio();
+
+function playAudio () {
+  audio.src = 'http://europa.shoutca.st:8648/;/;stream.mp3'
+  audio.play()
+}
+
+function stopAudio () {
+  audio.pause()
+  audio.src = ''
+  audio = new Audio()
+}
+
+function currentSongs () {
+  const songInfo = document.querySelector('#currentSong')
+  let content
+  if (typeof (content) === 'undefined') {
+    currentSection.style.display="none"
+  }
+  $.getJSON('https://europa.shoutca.st/rpc/atsueste/streaminfo.get', function (data) {
+    content = `<img id="cover" src=${data.data[0]['track']['imageurl']} alt="album cover"><ul>
+    <li class="artist">${data.data[0]['track']['artist']}</li>
+    <li class="songTitle">${data.data[0]['track']['title']}</li>
+    </ul>`
+    songInfo.innerHTML = content
+  })
+  currentSection.style.display="flex"
+}
+
+// Update song info every 4 seconds
+setInterval(currentSongs, 4000)
+
+function toggleAudio () {
+  if (player.getAttribute('src') === "images/Groove_Play_128.png") {
+    playAudio();
+    player.src = "images/Groove_Stop_128.png"
+  }
+  else if (player.getAttribute('src') === "images/Groove_Stop_128.png") {
+    stopAudio();
+    player.src = "images/Groove_Play_128.png"
+  }
+}
